6d67edc59217f5f6cfa82f41a429a562b2277283
chenglin <837460387@qq.com>
2018-10-25 21:12:35 +0800

    intialize

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="title">热门推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of list" :key="item.id">
+        <img :src="item.imgUrl" class="item-img">
+        <div class="item-info">
+          <div class="item-title">{{item.title}}</div>
+          <div class="item-desc">{{item.desc}}</div>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    line-height: .6rem
+    margin-top: .1rem
+    background: #eee
+    text-indent: .1rem
+  .item
+    height: 1.9rem
+    overflow: hidden
+    display: flex
+    .item-img
+      width: 1.7rem
+      height 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        color: #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="border-bottom" v-for="item of list" :key="item.id">
+        <div class="item-img-wrapper">
+          <img :src="item.imgUrl" class="item-img">
+        </div>
+        <div class="item-info">
+          <div class="item-title">{{item.title}}</div>
+          <div class="item-desc">{{item.desc}}</div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    line-height: .6rem
+    background: #eee
+    text-indent: .1rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 37.09%
+    .item-img
+      width: 100%
+      padding: .1rem
+  .item-info
+    padding: .1rem
+    min-width: 0
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+</style>
