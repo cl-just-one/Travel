38278d99ac77eeb294fe25abd9f90a10a376b667
chenglin <837460387@qq.com>
2018-10-25 21:41:36 +0800

    city router to search

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="请输入城市名或者拼音">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/variable.styl'
+  .search
+    height: .72rem
+    background: $bgColor
+    padding: 0 .3rem
+    .search-input
+      height: .62rem
+      width: 100%
+      padding: 0 .1rem
+      text-align: center
+      color: #666
+      border-radius: .06rem
+</style>
